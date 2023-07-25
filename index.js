ac351c0d4bb83a4ba949dcc7b3249e97f5db49fe
abhishek-pb1 <studentabhishekpb@gmail.com>
2023-07-25 17:22:53 +0530

    transition - solved

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ navToggler.addEventListener("click", () => {
 
     if(visbility === "false") {
         navMenu.setAttribute('data-visible', "true");
+        navToggler.style.top = '4.5vh';
+        navToggler.style.right = '3vh';
         // navMenu.style.height = "0"
         navToggler.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     }
 
     else if(visbility ==="true") {
         navMenu.setAttribute('data-visible', "false");
-        // navMenu.style.height = "100%"
         navToggler.innerHTML = '<i class="fa-solid fa-bars"></i>';
     }
 })
